92800e8afe86ae31b223da5b2c4315b5275160b4
lizbayrak <liz.bayrak@yahoo.co.uk>
2020-05-05 17:49:46 +0100

    add Login.java

---
+++ src/tests/Login.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is the log in class");
+
+        int a = 10;
+        int b = 20;
+    }
+}
