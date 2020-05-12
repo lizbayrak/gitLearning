2e87818290b690f9da3b39e3040f0c9802abc722
lizbayrak <liz.bayrak@yahoo.co.uk>
2020-05-12 15:43:50 +0100

    add Practice.java

---
+++ src/tests/Practice.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Practice");
+        System.out.println("Practice");
+        System.out.println("Practice");
+
+    }
+}
